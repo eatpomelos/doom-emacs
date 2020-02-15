a1ee18f2c35cffc391a95f1440289082612a2ad7
eatpomelos <2845748931@qq.com>
2020-02-14 20:53:52 -0800

    update

--- +org.el
+++ +org.el
@@ -4,12 +4,12 @@
 ;; (add-to-list 'auto-mode-alist '("\\.txt\\'" . org-mode))
 
 ;; 设置一下自己的任务管理的一些简单的配置,要是想放弃一个任务的时候，要进行说明，以后可能会再次启用这个任务
-(setq org-todo-keywords '((sequence "TODO(t)" "DOING(i)" "PAUSE(p@)" "|" "DONE(d)" "ABORT(a@/!)")))
-(setq org-todo-keyword-faces '(("TODO" . "red")
-                               ("DOING" . "yellow")
-                               ("DONE" . "green")
-                               ("ABORT" . "blue")
-                               ("PAUSE" . "cyan1")))
+;; (setq org-todo-keywords '((sequence "TODO(t)" "DOING(i)" "PAUSE(p@)" "|" "DONE(d)" "ABORT(a@/!)")))
+;; (setq org-todo-keyword-faces '(("TODO" . "red")
+;;                                ("DOING" . "yellow")
+;;                                ("DONE" . "green")
+;;                                ("ABORT" . "blue")
+;;                                ("PAUSE" . "cyan1")))
 
 ;; 定义一个函数,在需要编写英语单词的时候,打开本地的文件用来进行单词的补全
 (defun pomelo|load-localdictionary ()
